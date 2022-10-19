508ed899b94ba183dd6b51d580a4f535d7273739
Nicolas GOEMAN <nicolas.goemn@gmail.com>
2022-10-19 13:38:00 +0200

    feat: completing classes and improve input file reading (notebook)

--- google_hash_code_rcs/classes.py
+++ google_hash_code_rcs/classes.py
@@ -1,9 +1,14 @@
+from xmlrpc.client import Boolean
 
+# ================================== #
+#       ARCHITECTURE CLASSES         #
+# ================================== #
 
 class Binary:
-    def __init__(self) -> None:
+    def __init__(self, id) -> None:
         self.services = []
         self.engineers = []
+        self.id = id
     
     def add_engineer(self, engineer) -> None:  # pragma: no cover
         self.engineers.append(engineer)
@@ -11,26 +16,34 @@ class Binary:
     def remove_engineer(self, engineer) -> None:  # pragma: no cover
         self.engineers.remove(engineer)
     
+    def add_service(self, service) -> None:  # pragma: no cover
+        self.services.append(service)
+    
+    def remove_service(self, service) -> None:  # pragma: no cover
+        self.services.remove(service)
+    
     def get_n_services(self) -> int:  # pragma: no cover
         return len(self.services)
 
     def get_n_engineers(self) -> int:  # pragma: no cover
-        return len(self.engineers)
-    
+        return len(self.engineers)   
+
 
 class Service:
-    def __init__(self, binary) -> None:
+    def __init__(self, name, binary) -> None:
+        self.name = name
         self.binary = binary
         self.features = []
     
     def add_feature(self, feature) -> None:  # pragma: no cover
         self.features.append(feature)
+        print(self.features)
 
 
 class Feature:
     def __init__(self, services: list[Service], users: int, difficulty: int) -> None:
         self.services = []
-        for service in self.services:
+        for service in services:
             self.add_service(service)
 
         self.users = users
@@ -39,6 +52,13 @@ class Feature:
     def add_service(self, service: Service) -> None:  # pragma: no cover
         self.services.append(service)
         service.add_feature(self)
+    
+    def is_launched(self) -> Boolean:
+        return len(self.services) == 0
+
+# ================================== #
+#           ENGINEER CLASS           #
+# ================================== #
 
 class Engineer:
     def __init__(self, id) -> None:
@@ -47,3 +67,36 @@ class Engineer:
         self.current_task = None
         self.is_available = True
     
+# ================================== #
+#           TASKS CLASSES            #
+# ================================== #
+
+class ImplementFeature:
+    def __init__(self, feature: Feature, binary: Binary, engineer: Engineer) -> None:
+        self.feature = feature
+        self.binary = binary
+        self.engineer = engineer
+
+        self.total_days = self.compute_difficulty()
+        self.remaining_days = self.total_days
+        
+        self.binary.engineers.append(engineer)
+
+        self.engineer.current_task = self
+    
+    def compute_difficulty(self) -> int:
+        return self.feature.difficulty + len(self.binary.services) + len(self.binary.engineers)
+
+class Wait:
+    def __init__(self, days: int, engineer: Engineer) -> None:
+        self.engineer = engineer
+        
+        self.total_days = days
+        self.remaining_days = self.total_days
+
+        self.engineer.current_task = self
+
+
+
+
+
